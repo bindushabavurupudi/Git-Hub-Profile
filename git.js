e84b7cbd8c5835aa1035438b350396aba1787b93
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2025-07-10 19:05:30 +0530

    formatted

--- git.js
+++ git.js
@@ -1,13 +1,4 @@
-// ✅ 3. GitHub User Profile Viewer
-// 🔹 API: https://api.github.com/users/{username}
-// 📌 Task:
-// Input a GitHub username
-// Fetch and display:
-// Name
-// Avatar
-// Public Repos
-// Followers
-// 🔹 Bonus: Show error message if user not found (404)
+
 let info = document.getElementsByClassName("info");
 let user = document.getElementById("user")
 let btn = document.getElementById("btn");
@@ -19,13 +10,13 @@ let follow = document.getElementById("Followers")
 let clear = document.getElementById("clear");
 
 clear.addEventListener("click", () => {
-  user.value = "";
-  nam.innerText = "";
-  avatar.src = "";
-  pubRepo.innerText = "";
-  follow.innerText = "";
-  para.innerText = "";
-  info[0].classList.add("hide");
+    user.value = "";
+    nam.innerText = "";
+    avatar.src = "";
+    pubRepo.innerText = "";
+    follow.innerText = "";
+    para.innerText = "";
+    info[0].classList.add("hide");
 });
 
 
@@ -33,16 +24,16 @@ btn.addEventListener("click", () => {
     para.innerText = "Loading user info...";
     info[0].classList.remove("hide");
     fetch(`https://api.github.com/users/${user.value}`)
-    .then(response => response.json())
-    .then(data => {
-        nam.innerText = `User Name: ${data.name || "No name provided"}`;
-        avatar.src = data.avatar_url;
-        pubRepo.innerText = `Public Repos: ${data.public_repos ?? "Not available"}`;
-        follow.innerText = `Followers: ${data.followers ?? "Not available"}`;
-        para.classList.add("hide"); 
-    })
-    .catch(error => {
-        para.innerText = "Failed to fetch info.";
-        console.error("Error fetching data:", error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            nam.innerText = `User Name: ${data.name || "No name provided"}`;
+            avatar.src = data.avatar_url;
+            pubRepo.innerText = `Public Repos: ${data.public_repos ?? "Not available"}`;
+            follow.innerText = `Followers: ${data.followers ?? "Not available"}`;
+            para.classList.add("hide");
+        })
+        .catch(error => {
+            para.innerText = "Failed to fetch info.";
+            console.error("Error fetching data:", error);
+        });
 })
